12da45450b527f081d7839e45c44a1a69ce7d2f9
EduardRadeanu <ediradeanu12345@gmail.com>
2023-05-18 02:51:31 +0300

    Pagina principala

---
+++ Tap.Money v3/Tap.Money v3/Shared/Expense.cs
@@ -0,0 +1,18 @@
+namespace TAPPPPPP.Shared
+{
+    public class Expense
+    {
+
+        public decimal Zi { get; set; }
+
+        public decimal Saptamana { get; set; }
+
+        public decimal Luna { get; set; }
+
+        public decimal An { get; set; }
+
+        public string Descriere { get; set; }
+
+        public decimal Valoare { get; set; }
+    }
+}
