b6d462f2f8494ca509ed7711ce32477db403f79f
MaisaKime2 <cosmindanila2002@icloud.com>
2023-05-18 13:14:40 +0300

    interfata

---
+++ Tap.Money v3/TAPPPPPP/Server/Controllers/ExpenseController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TAPPPPPP.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TAPPPPPP.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReportExpensesController : ControllerBase
+    {
+        private static readonly string[] Expenses = new[]
+        {
+            "Cafea", "Supermarket", "Fuel", "Restaurant"
+        };
+
+        private readonly ILogger<ReportExpensesController> _logger;
+        private readonly List<Expense> _expenses;
+        private IEnumerable<Expense> expenses;
+
+        public ReportExpensesController(ILogger<ReportExpensesController> logger)
+        {
+            _logger = logger;
+            _expenses = new List<Expense>();
+        }
+
+        [HttpGet]
+        public IEnumerable<Expense> Get()
+        {
+            expenses = _expenses;
+            return expenses;
+        }
+
+
+        [HttpPost]
+        public IActionResult AddExpense([FromBody] Expense expense)
+        {
+            _expenses.Add(expense);
+            return Ok(_expenses);
+        }
+    }
+}
---
+++ Tap.Money v3/TAPPPPPP/Shared/Expense.cs
@@ -0,0 +1,19 @@
+namespace TAPPPPPP.Shared
+{
+    public class Expense
+{
+    public DateOnly Date { get; set; }
+
+    public decimal Zi { get; set; }
+
+    public decimal Saptamana { get; set; }
+
+    public decimal Luna { get; set; }
+
+    public decimal An { get; set; }
+
+    public string Descriere { get; set; }
+
+    public decimal Valoare { get; set; }
+}
+}
