e8fd2a496e4e630d4309849d0746cddfc6e0fcab
MaisaKime2 <cosmindanila2002@icloud.com>
2023-05-18 13:23:00 +0300

    versiunea finala

--- Tap.Money v3/TAPPPPPP/Shared/Expense.cs
+++ Tap.Money v3/TAPPPPPP/Shared/Expense.cs
@@ -1,19 +1,18 @@
 namespace TAPPPPPP.Shared
 {
     public class Expense
-{
-    public DateOnly Date { get; set; }
+    {
 
-    public decimal Zi { get; set; }
+        public decimal Zi { get; set; }
 
-    public decimal Saptamana { get; set; }
+        public decimal Saptamana { get; set; }
 
-    public decimal Luna { get; set; }
+        public decimal Luna { get; set; }
 
-    public decimal An { get; set; }
+        public decimal An { get; set; }
 
-    public string Descriere { get; set; }
+        public string Descriere { get; set; }
 
-    public decimal Valoare { get; set; }
-}
+        public decimal Valoare { get; set; }
+    }
 }
